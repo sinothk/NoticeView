459b2f3d5d2c90250ca757e0690382ee9afdfc51
LiangYT <381518188@qq.com>
2019-02-20 09:26:52 +0800

    暂存！

---
+++ app/src/main/java/com/sinothk/noticeView/demo/NoticeViewDemoMainActivity.java
@@ -0,0 +1,13 @@
+package com.sinothk.noticeView.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticeViewDemoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_view_demo_main);
+    }
+}
